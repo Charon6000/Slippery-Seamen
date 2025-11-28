92989bbbd6baf19e067f1b12ced5d735bfe3f905
Charon6000 <hubertwit2005@gmail.com>
2025-11-28 17:50:21 +0100

    łączanie fronta z backendem i pierwszy model

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from tensorflow.keras import layers, models 
 
 app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+CORS(app)
+@app.route("/", methods=["POST"])
+def upload_data():
+    return "Hello, World!"
+    
+    # return jsonify({"mouse_bite": {models.load_model("mouse_bite_model.h5").predict(request.files.get("files"))},
+    #                 "spur": {models.load_model("spur_model.h5").predict(request.files.get("files"))},
+    #                 "missing_hole": {models.load_model("missing_hole_model.h5").predict(request.files.get("files"))},
+    #                 "short": {models.load_model("short_model.h5").predict(request.files.get("files"))},
+    #                 "opencircut": {models.load_model("opencircut_model.h5").predict(request.files.get("files"))},
+    #                 "spurious_copper": {models.load_model("spurious_copper_model.h5").predict(request.files.get("files"))},})
--- backend/train.py
+++ backend/train.py
@@ -5,7 +5,6 @@ from tensorflow.keras import layers, models  # type: ignore
 from tensorflow.keras.models import Sequential
 
 
-
 base_path = Path(__file__).resolve().parent / "training_data"
 IMG_SIZE=(32, 32)
 train_ds, val_ds, categories = prepare(base_path, IMG_SIZE)
@@ -15,25 +14,13 @@ num_classes = len(categories)
 train_ds, val_ds, categories = prepare(base_path, IMG_SIZE)
 
 num_classes = len(categories)
-
-model = Sequential([
-  # usuwamy Rescaling tutaj — dane już znormalizowane w prepare()
-  layers.Conv2D(2, 3, padding='same', activation='relu', input_shape=(*IMG_SIZE, 3)),
-  layers.MaxPooling2D(),
-  layers.Conv2D(4, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(8, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
-  layers.Dense(16, activation='relu'),
-  layers.Dense(num_classes, activation='softmax')
-])
+model = models.load_model(Path(__file__).resolve().parent / "models/spur_model.h5")
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 model.fit(
   train_ds,
   validation_data=val_ds,
-  epochs=1
+  epochs=100
 )
 model.save(Path(__file__).resolve().parent / "models/spur_model.h5")
