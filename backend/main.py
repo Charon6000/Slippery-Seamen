61a6180e596c3823fd36529cebc3b912dec9d154
Charon6000 <hubertwit2005@gmail.com>
2025-11-28 22:29:38 +0100

    UI update

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,21 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from tensorflow.keras import layers, models 
+# from tensorflow.keras import layers, models 
 
 app = Flask(__name__)
 CORS(app)
 @app.route("/", methods=["POST"])
 def upload_data():
-    return jsonify({"file_name", "nazwa",
-                    "mouse_bite": 1,
-                    "spur": 1,
-                    "missing_hole": 1,
-                    "short": 1,
-                    "opencircut": 1,
-                    "spurious_copper": 1,})
+    return jsonify({
+        "results": [{
+                "file_name": request.files.get("files").name,
+                "mouse_bite": 1,
+                "spur": 0,
+                "missing_hole": 1,
+                "short": 1,
+                "opencircut": 0,
+                 "spurious_copper": 1,
+             }]})
                     
     # return jsonify({"mouse_bite": {models.load_model("mouse_bite_model.h5").predict(request.files.get("files"))},
     #                 "spur": {models.load_model("spur_model.h5").predict(request.files.get("files"))},
