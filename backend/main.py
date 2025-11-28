75c8c4a5dba5cac807542c7a4a288e35f934cfc1
Charon6000 <hubertwit2005@gmail.com>
2025-11-28 18:37:24 +0100

    dodanie wstepnych danych do wyswietlania

--- backend/main.py
+++ backend/main.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 CORS(app)
 @app.route("/", methods=["POST"])
 def upload_data():
-    return "Hello, World!"
-    
+    return jsonify({"mouse_bite": 1,
+                    "spur": 1,
+                    "missing_hole": 1,
+                    "short": 1,
+                    "opencircut": 1,
+                    "spurious_copper": 1,})
     # return jsonify({"mouse_bite": {models.load_model("mouse_bite_model.h5").predict(request.files.get("files"))},
     #                 "spur": {models.load_model("spur_model.h5").predict(request.files.get("files"))},
     #                 "missing_hole": {models.load_model("missing_hole_model.h5").predict(request.files.get("files"))},
